0ce04378e35b9a5e91ba5d024634324182ecd5d7
Georgios Zervos <papadako@csd.uoc.gr>
2023-01-10 13:14:10 +0200

    test

---
+++ JSONlang.h
@@ -0,0 +1,45 @@
+//
+// Created by Georgios Zervos on 7/1/23.
+//
+
+#ifndef CS352_JSONLANG_H
+#define CS352_JSONLANG_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include <map>
+
+#define PROGRAM_BEGIN ;int main(){
+#define PROGRAM_END ;return 0;}
+
+#define JSON(name) ;auto name
+
+#define STRING(text) Object<std::string>(text)
+#define NUMBER(x)  Object<float>(x)
+
+#define OBJECT std::vector <std::string>
+
+#define KEY(name)  "#name" , false ? ""
+
+#define TRUE Object<bool>(true);
+#define FALSE Object<bool>(false);
+
+class O{};
+
+template<typename T>
+class Object : public O{
+public:
+    T value;
+    Object(){}
+
+    Object(T val){ value = val;}
+
+};
+
+
+
+
+#endif //CS352_JSONLANG_H
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "JSONlang.h"
+        JSON(string) = STRING("HELLO WORLD ")
+        JSON(num) = NUMBER(69)
+        JSON(flo) = NUMBER(69.665)
+        JSON(OBJE) = OBJECT {
+         KEY("test") :  " "  ,
+        }
+
+PROGRAM_BEGIN
+
+
+
+PROGRAM_END
