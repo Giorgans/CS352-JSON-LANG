311695327e202269800321c21f92ad418b3331e7
Georgios Zervos <papadako@csd.uoc.gr>
2023-01-11 04:45:44 +0200

    fixed syntax

--- JSONlang.h
+++ JSONlang.h
@@ -7,33 +7,45 @@
 #include <algorithm>
 #include <memory>
 #include <map>
-
+bool isObject = false;
 #define PROGRAM_BEGIN ;int main(){
 #define PROGRAM_END ;return 0;}
 
 #define JSON(name) ;auto name
 
-#define STRING(text) Object<std::string>(text)
-#define NUMBER(x)  Object<float>(x)
+// Object definitions
+#define STRING(text) Object(STRING,text)
+#define NUMBER(x)    Object(getNumberType(x),std::to_string(x))
+#define OBJECT std::vector <Object>
+#define KEY(name) STRING(name) , (false) ? STRING("NULL")
+#define ARRAY  std::vector <Object>
+#define TRUE Object(BOOL,"true");
+#define FALSE Object(BOOL,"false");
 
-#define OBJECT std::vector <std::string>
+// Editing definitions
+#define SET
+#define ASSIGN =
+#define ERASE
 
-#define KEY(name)  "#name" , false ? ""
+//Output
+#define PRINT
 
-#define TRUE Object<bool>(true);
-#define FALSE Object<bool>(false);
-
-class O{};
-
-template<typename T>
-class Object : public O{
+typedef enum type { INT , FLOAT , STRING , BOOL , NUL }Type;
+class Object {
 public:
-    T value;
+    std::string value;
+    Type type;
     Object(){}
-
-    Object(T val){ value = val;}
-
+    Object(Type t,std::string val):value(val),type(t){}
+    ~Object();
 };
+
+Type getNumberType(float x){
+    if (abs(x- int(x) > 0) )
+        return FLOAT;
+    else
+        return INT;
+}
 
 
 
--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
         JSON(string) = STRING("HELLO WORLD ")
         JSON(num) = NUMBER(69)
         JSON(flo) = NUMBER(69.665)
-        JSON(OBJE) = OBJECT {
-         KEY("test") :  " "  ,
+        JSON(boolt) = TRUE
+        JSON(boolf) = FALSE
+        JSON(ob) = OBJECT {
+            KEY("test1")  : STRING("value") ,
+            KEY("test2")  : NUMBER(69) ,
+            KEY("test3")  : NUMBER(69.665)
         }
-
 PROGRAM_BEGIN
 
-
-
+    std::cout << string.value  << std::endl << num.value  << std::endl << flo.value  << std::endl;
 PROGRAM_END
